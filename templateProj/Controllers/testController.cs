be301e09f818132c6c40a961fbc4e63fff747c98
devni <devniheraliyawala314@gmail.com>
2017-07-17 00:36:20 +0530

    New ActionReult 'testBranch' has been added to test the connection of the 'development' branch.

--- templateProj/Controllers/testController.cs
+++ templateProj/Controllers/testController.cs
@@ -13,5 +13,9 @@ namespace templateProj.Controllers
         {
             return View();
         }
+        public ActionResult testBranch()
+        {
+            return View();
+        }
     }
 }
